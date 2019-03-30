21e4915f2c1efda4a54b72e699b099b5294c1cf6
JDC-RVA <jeremy@firstmotionlc.com>
2019-03-30 11:33:24 -0400

    updated app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,7 @@
-// var queryURL = "https://feeds.citibikenyc.com/stations/stations.json";
-// var queryURL = "https://proxy.streamdata.io/https://feeds.citibikenyc.com/stations/stations.json";
 var queryURL = "http://api.citybik.es/v2/networks";
 function ajaxCall () {
 
-
+// ajax call to citybike api
 $.ajax({
   url: queryURL,
   method: "GET"
@@ -12,4 +10,33 @@ $.ajax({
 });
 }
 
+// calling the ajaxCall function
 ajaxCall()
+
+// firebase database setup
+var config = {
+  apiKey: "AIzaSyAM1poplPY_SUqYYanI-ZwgAqrK0z_YnSM",
+  authDomain: "bike-find-international.firebaseapp.com",
+  databaseURL: "https://bike-find-international.firebaseio.com",
+  projectId: "bike-find-international",
+  storageBucket: "bike-find-international.appspot.com",
+  messagingSenderId: "556431147211"
+};
+firebase.initializeApp(config);
+
+// A variable to reference the database.
+var database = firebase.database();
+
+var location;
+
+$("#search").on("click", function() {
+  event.preventDefault();
+  // Storing and retreiving location data
+  location = $("#user-input").val().trim();
+  
+  // Pushing to database
+  database.ref().push({
+      location: location,
+  });
+  $("form")[0].reset();
+});
