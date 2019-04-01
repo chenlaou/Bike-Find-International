4d37fa5458ab3257990d040b117c60aa76d34fc1
JDC-RVA <jeremy@firstmotionlc.com>
2019-04-01 19:00:39 -0400

    added ajax call to citybike API and connected to firebase.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,12 @@
-var queryURL = "http://api.citybik.es/v2/networks";
-function ajaxCall () {
-
-// ajax call to citybike api
-$.ajax({
-  url: queryURL,
-  method: "GET"
-}).then(function(response) {
-  console.log(response);
-});
-}
-
-// calling the ajaxCall function
-ajaxCall()
-
-// firebase database setup
+$(document).ready(function(){
+  // firebase database setup
 var config = {
   apiKey: "AIzaSyAM1poplPY_SUqYYanI-ZwgAqrK0z_YnSM",
-  authDomain: "bike-find-international.firebaseapp.com",
-  databaseURL: "https://bike-find-international.firebaseio.com",
-  projectId: "bike-find-international",
-  storageBucket: "bike-find-international.appspot.com",
-  messagingSenderId: "556431147211"
+    authDomain: "bike-find-international.firebaseapp.com",
+    databaseURL: "https://bike-find-international.firebaseio.com",
+    projectId: "bike-find-international",
+    storageBucket: "bike-find-international.appspot.com",
+    messagingSenderId: "556431147211"
 };
 firebase.initializeApp(config);
 
@@ -33,10 +19,28 @@ $("#search").on("click", function() {
   event.preventDefault();
   // Storing and retreiving location data
   location = $("#user-input").val().trim();
-  
+  console.log(location)
+  var newSearch = {
+    location: location,
+  }
+  console.log(newSearch)
   // Pushing to database
-  database.ref().push({
-      location: location,
+  database.ref().set(newSearch)
+});
+
+// CITYBIKE API
+var queryURL = "http://api.citybik.es/v2/networks";
+function ajaxCall() {
+
+  // ajax call to citybike api
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response);
   });
-  $("form")[0].reset();
+}
+
+// calling the ajaxCall function
+ajaxCall()
 });
